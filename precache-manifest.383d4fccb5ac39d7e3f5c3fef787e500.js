8702b79eaa09b5eaf7e8212fad49a1f1798a51d3
fdcoder81 <carloanselmi81@gmail.com>
2020-03-28 16:18:01 +0000

    Updates

---
+++ precache-manifest.383d4fccb5ac39d7e3f5c3fef787e500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c64c0b0a9a4089dd7d58a3a4ea0f4334",
+    "url": "/clothing-shop/index.html"
+  },
+  {
+    "revision": "35fec11a4115afc95ac6",
+    "url": "/clothing-shop/static/css/main.e3e7d8df.chunk.css"
+  },
+  {
+    "revision": "417bda15b7b3f4c4282a",
+    "url": "/clothing-shop/static/js/2.0edbc99c.chunk.js"
+  },
+  {
+    "revision": "35fec11a4115afc95ac6",
+    "url": "/clothing-shop/static/js/main.3aaf7630.chunk.js"
+  },
+  {
+    "revision": "ff03d3b57b366662af98",
+    "url": "/clothing-shop/static/js/runtime~main.1ffd74ad.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/clothing-shop/static/media/cart-icon.30f07071.svg"
+  },
+  {
+    "revision": "9b1230dc8e8fbbcf1a78a4324c7829b9",
+    "url": "/clothing-shop/static/media/logo.9b1230dc.png"
+  }
+]);
