38b8818733ffd5d5b4fa49d2aae1fc570a2d8848
fdcoder81 <carloanselmi81@gmail.com>
2020-03-30 10:41:31 +0100

    Added CartItem component to display items to the cart

--- src/components/cart-dropdown/CartDropDown.js
+++ src/components/cart-dropdown/CartDropDown.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import "./cartDropdown.scss";
 import CustomButton from "../custom-button/CustomButton";
+import CartItem from "../cart-item/CartItem";
 
-const CartDropDown = () => {
+const CartDropDown = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map(cartItem => {
+          return <CartItem key={cartItem.id} item={cartItem} />;
+        })}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default CartDropDown;
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  return {
+    cartItems
+  };
+};
+
+export default connect(mapStateToProps)(CartDropDown);
